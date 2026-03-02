63577fa70f91392dca3baf8cf0d145f8e75a3d48
citiunionbank02-ui <citiunionbank02@gmail.com>
2026-03-02 20:00:55 +0100

    Create jitoBundle.ts file with Jito bundle transaction code

---
+++ src/utils/jitoBundle.ts
@@ -0,0 +1,8 @@
+// Jito Bundle Transaction Code
+
+// This is a placeholder for the actual Jito bundle transaction code implementation.
+
+export const jitoBundleTransaction = () => {
+    // Implement your transaction logic here
+    console.log('Executing Jito bundle transaction.');
+};
